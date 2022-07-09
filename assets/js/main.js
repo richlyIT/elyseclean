6a5b706b8a42fc4906e9f318cb03cca92ff77dbc
NeoLucem <98679519+NeoLucem@users.noreply.github.com>
2022-07-09 15:18:07 +0000

    add of services section

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,21 +1,20 @@
-/*
 //Services section
 //Card
 let menu = [
     {
-        image: "/assets/images/1.jpg",
+        image: "assets/images/1.jpg",
         title: "House Cleaning",
         text: "Forget cleaning, enjoy the benefits of a full-time housekeeper without the cost, this is the most commonly requested service for families, busy professionals & those who value their time, we’ll keep your house clean & fresh.",
         link: "",
     },
     {
-        image: "/assets/images/cardImage1.jpg",
+        image: "assets/images/cardimage1.jpg",
         title: "Move Out/In Cleaning",
         text: "This service is extremely thorough top to bottom, designed to prepare a home for a new tenant. This service includes cleaning Inside Kitchen Cabinets, Inside Oven & Fridge, Interior Windows & Sills, Detail Blinds & Detail Baseboards.",
         link: "",
     },
     {
-        image: "/assets/images/cardImage2.jpg",
+        image: "assets/images/cardimage2.jpg",
         title: "Deep Cleaning",
         text: "Ideal for homes that have gone months without being cleaned or have NOT been professionally cleaned in a while. This service includes cleaning window sills, hand detail blinds & hand detail baseboards.",
         link: "",
@@ -44,5 +43,3 @@ window.addEventListener("DOMContentLoaded", function(){
     cardSection.innerHTML = displayCard
     console.log(displayCard);
 });
-
-*/
