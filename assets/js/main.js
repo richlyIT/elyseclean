848c451fcef9ab58b8cea56e2874d99b0fe5b62e
NeoLucem <98679519+NeoLucem@users.noreply.github.com>
2022-07-14 15:35:27 +0000

    add of services page and submit form

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,19 +5,19 @@ let menu = [
         image: "assets/images/1.jpg",
         title: "House Cleaning",
         text: "Forget cleaning, enjoy the benefits of a full-time housekeeper without the cost, this is the most commonly requested service for families, busy professionals & those who value their time, we’ll keep your house clean & fresh.",
-        link: "",
+        link: "./assets/pages/services/home_cleaning.html",
     },
     {
         image: "./assets/images/cardimage1.jpg",
         title: "Move Out/In Cleaning",
         text: "This service is extremely thorough top to bottom, designed to prepare a home for a new tenant. This service includes cleaning Inside Kitchen Cabinets, Inside Oven & Fridge, Interior Windows & Sills, Detail Blinds & Detail Baseboards.",
-        link: "",
+        link: "./assets/pages/services/move_cleaning.html",
     },
     {
         image: "./assets/images/cardimage2.jpg",
         title: "Deep Cleaning",
         text: "Ideal for homes that have gone months without being cleaned or have NOT been professionally cleaned in a while. This service includes cleaning window sills, hand detail blinds & hand detail baseboards.",
-        link: "",
+        link: "./assets/pages/services/deep_cleaning.html",
     }
 ];
 
@@ -34,13 +34,13 @@ window.addEventListener("DOMContentLoaded", function(){
               <div class="card-body">
                 <h5 class="card-title">${item.title}</h5>
                 <p class="card-text">${item.text}</p>
-                <a href="#" class="btn btn-card"><i class="bi bi-plus-circle"></i>More</a>
+                <a href=${item.link} class="btn btn-card"><i class="bi bi-plus-circle"></i>More</a>
               </div>
             </div>
           </div>`;
     });
     displayCard = displayCard.join("")
-    cardSection.innerHTML = displayCard
+    cardSection.innerHTML = displayCard;
     console.log(displayCard);
 });
 
@@ -52,3 +52,5 @@ $(document).ready(function(){
         $('nav').toggleClass('scrolled', $(this).scrollTop() > 100);
       });  
 });  
+
+
